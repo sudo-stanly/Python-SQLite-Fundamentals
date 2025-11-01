52ad5b5d023571f725b95680f0dae296d1d3b033
sudo-stanly <alys.git24@gmail.com>
2025-11-02 01:06:05 +0800

    ../

--- Fundamentals/database.py
+++ Fundamentals/database.py
@@ -1,19 +1,17 @@
 import sqlite3
 
-conn = sqlite3.connect('customer.db')
+#  query the database and return all recods
+def show_all():
+    # connect to database
+    conn = sqlite3.connect('./Fundamentals/customer.db')
+    #create a cursor
+    c = conn.cursor()
 
-#create a cursor
-c = conn.cursor()
+    c.execute("SELECT rowid,* FROM customers")
+    items = c.fetchall()
+    for item in items:
+        print(item)
 
-#query database
-c.execute("SELECT rowid,* FROM customers")
-items = c.fetchall()
-for item in items:
-    print(item)
-
-
-
-
-print("\ncommand executed sucessfully...")
-conn.commit()
-conn.close()
+    print("\ncommand executed sucessfully...")
+    conn.commit()
+    conn.close()
