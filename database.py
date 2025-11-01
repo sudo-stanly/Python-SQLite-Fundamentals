db8517ed2634e70a0b7ba437c5637597003ccf49
sudo-stanly <alys.git24@gmail.com>
2025-11-01 09:22:45 +0800

    adding many records into table

--- database.py
+++ database.py
@@ -6,8 +6,14 @@ conn = sqlite3.connect('customer.db')
 #create a cursor
 c = conn.cursor()
 
-#insert a record in table
-c.execute("INSERT INTO customers VALUES('Mary','Poppins','mpoppins@email.com')")
+#insert many record in table
+many_customers = [
+    ('West', 'Brown', 'wes@brown.com'),
+    ('Steph','Keuwa','steph@keuwa.com'),
+    ('Dan','Pas','danpas@pas.com')
+]
+c.executemany("INSERT INTO customers VALUES(?,?,?)", many_customers)
+
 
 print("command executed sucessfully...")
 conn.commit()
