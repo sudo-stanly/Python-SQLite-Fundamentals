aba8ce0cb7a7ff5fe9c7aae108447c43f3383768
sudo-stanly <alys.git24@gmail.com>
2025-11-01 10:01:33 +0800

    querying and fetching database

--- database.py
+++ database.py
@@ -6,13 +6,11 @@ conn = sqlite3.connect('customer.db')
 #create a cursor
 c = conn.cursor()
 
-#insert many record in table
-many_customers = [
-    ('West', 'Brown', 'wes@brown.com'),
-    ('Steph','Keuwa','steph@keuwa.com'),
-    ('Dan','Pas','danpas@pas.com')
-]
-c.executemany("INSERT INTO customers VALUES(?,?,?)", many_customers)
+#query the database
+c.execute("SELECT * FROM customers")
+# c.fetchone()
+# c.fetchmany(3)
+print(c.fetchall())
 
 
 print("command executed sucessfully...")
