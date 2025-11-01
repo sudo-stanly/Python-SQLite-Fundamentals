39735a3609cb4c8fcfd1489f2f6cfa74608586a3
sudo-stanly <alys.git24@gmail.com>
2025-11-01 13:35:12 +0800

    dropping a table

--- database.py
+++ database.py
@@ -5,8 +5,12 @@ conn = sqlite3.connect('customer.db')
 #create a cursor
 c = conn.cursor()
 
+#drop a table
+c.execute("DROP TABLE customers")
+conn.commit()
+
 #query database
-c.execute("SELECT rowid,* FROM customers ORDER BY rowid DESC LIMIT 2")
+c.execute("SELECT rowid,* FROM customers")
 items = c.fetchall()
 for item in items:
     print(item)
